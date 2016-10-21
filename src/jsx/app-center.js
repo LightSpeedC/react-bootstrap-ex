e3fc16d8a99621150f4a179f3f0a9d6212087506
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-21 16:08:08 +0900

    bootstrap utton

---
+++ src/jsx/app-center.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function handleClick() {
+	alert('onClick triggered on the title component');
+}
+
+const AppCenter = props =>
+		<div>
+			app center
+		</div>;
+
+export default AppCenter;
