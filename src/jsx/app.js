acaf5d57843b282017920bdc335706afef9ee7ee
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-21 16:08:21 +0900

    bootstrap button

--- src/jsx/app.js
+++ src/jsx/app.js
@@ -8,11 +8,36 @@ require('react-tap-event-plugin')();
 
 import AppHeader from './app-header';
 import AppFooter from './app-footer';
+import AppCenter from './app-center';
+import {Button, ButtonToolbar} from 'react-bootstrap';
 
 const MyAwesomeReactComponent = props =>
 	<div>
 		<AppHeader />
+		<AppCenter />
 		<p>test p</p>
+  <ButtonToolbar>
+    {/* Standard button */}
+    <Button>Default</Button>
+
+    {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+    <Button bsStyle="primary">Primary</Button>
+
+    {/* Indicates a successful or positive action */}
+    <Button bsStyle="success">Success</Button>
+
+    {/* Contextual button for informational alert messages */}
+    <Button bsStyle="info">Info</Button>
+
+    {/* Indicates caution should be taken with this action */}
+    <Button bsStyle="warning">Warning</Button>
+
+    {/* Indicates a dangerous or potentially negative action */}
+    <Button bsStyle="danger">Danger</Button>
+
+    {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+    <Button bsStyle="link">Link</Button>
+  </ButtonToolbar>
 		<AppFooter />
 	</div>;
 
