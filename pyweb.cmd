e650d005dbb4a909f4df7efc8f2e695c42e52d17
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-22 15:49:33 +0900

    change port

--- pyweb.cmd
+++ pyweb.cmd
@@ -1,4 +1,5 @@
+if "%PORT%" == "" set PORT=3000
 pushd dist
-start python -m SimpleHTTPServer 3001
-start http://localhost:3001/
+start python -m SimpleHTTPServer %PORT%
+start http://localhost:%PORT%/
 popd
