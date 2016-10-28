eb08f18a266d78dd919e33747db4917cccb5da02
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-28 20:40:08 +0900

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,19 +10,11 @@ const browserify = require('browserify');
 const babelify   = require('babelify');
 const literalify = require('literalify');
 const source     = require('vinyl-source-stream');
-const run        = require('run-sequence');
+//const run        = require('run-sequence');
 const notify     = require('gulp-notify');
 
-const SRC_FILES = ['src/**/*.html', 'src/**/*.css', 'src/**/*.ico'];
-
-gulp.task('default', ['build', 'copy-files', 'copy-min-js'],
-	cb => run('watch', 'watch-files', cb));
-
-gulp.task('build',
-	cb => run('browserify', 'uglify', cb));
-
-gulp.task('watch', () =>
-	gulp.watch('src/xyz/jsx/*.js', ['build']));
+const SRC_FILES = ['src/**/*.html', 'src/**/*.css', 'src/**/*.ico', 'src/**/*.js'];
+const JSX_FILES = ['src/xyz/jsx/*.js'];
 
 gulp.task('browserify', () =>
 	browserify({debug: true})
@@ -40,18 +32,18 @@ gulp.task('browserify', () =>
 		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
 		.pipe(source('bundle.js'))
 		.pipe(gulp.dest('dist/xyz/js/'))
-		.pipe(notify({message:'ブラウザリファイ終了', title:'Gulp'}))
+		.pipe(notify({message:'Browserify終了', title:'Gulp'}))
 );
 
-gulp.task('uglify', () =>
+gulp.task('uglify', ['browserify'], () =>
 	gulp.src('dist/xyz/js/bundle.js')
-		.pipe(uglify())
+		.pipe(uglify({preserveComments: 'license'}))
 		.pipe(rename('bundle.min.js'))
-		.pipe(gulp.dest('dist/xyz/js/')));
-//		.pipe(uglify({preserveComments: 'license'}))
+		.pipe(gulp.dest('dist/xyz/js/'))
+		.pipe(notify({message:'Uglify終了', title:'Gulp'})));
 
-gulp.task('watch-files', () =>
-	gulp.watch(SRC_FILES, ['copy-files']));
+gulp.task('build', ['uglify'], () => 
+	notify({message:'build終了', title:'Gulp'}));
 
 gulp.task('copy-files', () =>
 	gulp.src(SRC_FILES)
@@ -63,7 +55,21 @@ gulp.task('copy-min-js', () =>
 			'node_modules/react-router/umd/*.min.js*',
 			'node_modules/react-bootstrap/dist/*.min.js*'])
 		.pipe(gulp.dest('dist/js')));
+//		.pipe(notify({message:'copy-min-js終了', title:'Gulp'})));
 
+gulp.task('first-build', ['build', 'copy-files', 'copy-min-js']);
+
+gulp.task('watch-jsx', ['first-build'], () =>
+	gulp.watch(JSX_FILES, ['build']));
+
+gulp.task('watch-files', ['first-build'], () =>
+	gulp.watch(SRC_FILES, ['copy-files']));
+
+gulp.task('watch-start', ['watch-jsx', 'watch-files']);
+
+gulp.task('default', ['watch-start']);
+
+/*
 const http = require('http');
 const fs   = require('fs');
 const path = require('path');
@@ -75,3 +81,4 @@ gulp.task('web', cb =>
 		.pipe(res)
 		.on('error', err => (res.end(err + ''), console.error(err)))
 	).listen(process.env.PORT || 3000, cb));
+*/
