fc3736b81db25097a6b2fb7798c618e3572e524d
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-28 20:40:19 +0900

    revise

--- src/xyz/jsx/app.js
+++ src/xyz/jsx/app.js
@@ -4,22 +4,56 @@ import React from 'react';
 import {render} from 'react-dom';
 import 'regenerator-runtime/runtime';
 
-// Needed for onTouchTap http://stackoverflow.com/a/34015469/988941
-//require('react-tap-event-plugin')();
-
 import AppHeader from './app-header';
 import AppFooter from './app-footer';
 import AppCenter from './app-center';
 
-const MyAwesomeReactComponent = props =>
+/*
+const App = props =>
 	<div>
 		<AppHeader />
 		<AppCenter />
-		<p>test p</p>
+		<p>test p2</p>
+		{props.children}
 		<AppFooter />
 	</div>;
+*/
 
-const App = props =>
-	<MyAwesomeReactComponent />;
+class MyComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		if (this.state === undefined) this.state = {};
+		console.log('My1:', Object.getOwnPropertyNames(this));
+		console.log('My2:', Object.getOwnPropertyNames(this.constructor.prototype));
+		Object.getOwnPropertyNames(this.constructor.prototype)
+			.filter(p => p.startsWith('on') || p.startsWith('handle'))
+			.forEach(p => Object.defineProperty(this, p,
+				{configurable: true, value: this[p].bind(this)}));
+	}
+}
 
-render(<App />, document.getElementById('app'));
+class App extends MyComponent {
+	constructor(props) {
+		super(props);
+		console.log('App1:', Object.getOwnPropertyNames(this));
+		console.log('App2:', Object.getOwnPropertyNames(this.constructor.prototype));
+		//this.state = {counter: 0};
+		//this.onClick = this.onClick.bind(this);
+	}
+	onClick() {
+		//this.setState({counter: this.state.counter + 1 || 1});
+		this.setState(state => ({counter: state.counter + 1 || 1}));
+	}
+	render() {
+		return <div>
+			<button onClick={this.onClick}>++{this.state.counter}</button>
+			<AppHeader />
+			<AppCenter />
+			<p>test p2</p>
+			{this.props.children}
+			<AppFooter />
+		</div>;
+	}
+}
+
+render(<App>hello world</App>, document.getElementById('app'));
