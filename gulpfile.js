9a40bfdd60588d8e3cfcafa4c466337e75a82e52
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-29 15:13:35 +0900

    revise

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+console.log('gulp starting...');
+
 const gulp       = require('gulp');
 const plumber    = require('gulp-plumber');
 const uglify     = require('gulp-uglify');
@@ -10,75 +12,76 @@ const browserify = require('browserify');
 const babelify   = require('babelify');
 const literalify = require('literalify');
 const source     = require('vinyl-source-stream');
-//const run        = require('run-sequence');
 const notify     = require('gulp-notify');
+const notifier   = require('node-notifier');
 
-const SRC_FILES = ['src/**/*.html', 'src/**/*.css', 'src/**/*.ico', 'src/**/*.js'];
-const JSX_FILES = ['src/xyz/jsx/*.js'];
+const X = ['xyz'];
+const SRC_FILES = [
+		'src/**/*.html',
+		'src/**/*.css',
+		'src/**/*.ico',
+		'src/**/*.js'];
+const MIN_FILES = [
+		'node_modules/react/dist/*.min.js*',
+		'node_modules/react-dom/dist/*.min.js*',
+		'node_modules/react-router/umd/*.min.js*',
+		'node_modules/react-bootstrap/dist/*.min.js*'];
 
-gulp.task('browserify', () =>
-	browserify({debug: true})
-		.on('error', err => console.log('eh!?', err))
-		.transform(babelify.configure({presets: ['es2015', 'react']}))
-		.transform(literalify.configure({
-			'react': 'window.React',
-			'react-dom': 'window.ReactDOM',
-			'react-router': 'window.ReactRouter',
-			'react-bootstrap': 'window.ReactBootstrap'
-		}))
-		.require('src/xyz/jsx/app.js', {entry: true})
-		.bundle()
-		.on('error', err => console.log('eh!?', err))
-		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-		.pipe(source('bundle.js'))
-		.pipe(gulp.dest('dist/xyz/js/'))
-		.pipe(notify({message:'Browserify終了', title:'Gulp'}))
-);
+gulp.task('browserify', X.map(x => 'browserify-' + x));
 
-gulp.task('uglify', ['browserify'], () =>
-	gulp.src('dist/xyz/js/bundle.js')
-		.pipe(uglify({preserveComments: 'license'}))
-		.pipe(rename('bundle.min.js'))
-		.pipe(gulp.dest('dist/xyz/js/'))
-		.pipe(notify({message:'Uglify終了', title:'Gulp'})));
+X.forEach(x => {
 
-gulp.task('build', ['uglify'], () => 
-	notify({message:'build終了', title:'Gulp'}));
+	gulp.task('browserify-' + x, () =>
+		browserify({debug: true})
+			.on('error', err => console.log('eh!?', err))
+			.transform(babelify, {presets: ['es2015', 'react']})
+			.transform(literalify, {
+				'react': 'window.React',
+				'react-dom': 'window.ReactDOM',
+				'react-router': 'window.ReactRouter',
+				'react-bootstrap': 'window.ReactBootstrap'
+			})
+			.require('src/' + x + '/jsx/app.js', {entry: true})
+			.bundle()
+			.on('error', err => console.log('eh!?', err))
+			.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+			.pipe(source('bundle.js'))
+			.pipe(gulp.dest('dist/' + x + '/js/')));
 
-gulp.task('copy-files', () =>
-	gulp.src(SRC_FILES)
-		.pipe(gulp.dest('dist/')));
+	gulp.task('uglify-' + x, ['browserify-' + x], () =>
+		gulp.src('dist/' + x + '/js/bundle.js')
+			.pipe(uglify({preserveComments: 'license'}))
+			.pipe(rename('bundle.min.js'))
+			.pipe(gulp.dest('dist/' + x + '/js/')));
+
+	gulp.task('build-jsx-' + x, ['uglify-' + x], () =>
+		notifier.notify({message:'Build-' + x + '終了', title:'Gulp'}));
+
+});
+
+gulp.task('build-jsx', X.map(x => 'build-jsx-' + x));
+
+SRC_FILES.forEach(file =>
+	gulp.task('copy-' + file, () =>
+		gulp.src(file)
+			.pipe(gulp.dest('dist/'))));
+
+gulp.task('copy-files', SRC_FILES.map(x => 'copy-' + x));
 
 gulp.task('copy-min-js', () =>
-	gulp.src(['node_modules/react/dist/*.min.js*',
-			'node_modules/react-dom/dist/*.min.js*',
-			'node_modules/react-router/umd/*.min.js*',
-			'node_modules/react-bootstrap/dist/*.min.js*'])
+	gulp.src(MIN_FILES)
 		.pipe(gulp.dest('dist/js')));
-//		.pipe(notify({message:'copy-min-js終了', title:'Gulp'})));
 
-gulp.task('first-build', ['build', 'copy-files', 'copy-min-js']);
+gulp.task('build-all', ['build-jsx', 'copy-files', 'copy-min-js']);
 
-gulp.task('watch-jsx', ['first-build'], () =>
-	gulp.watch(JSX_FILES, ['build']));
+gulp.task('watch-jsx', ['build-all'], () =>
+	X.forEach(x =>
+		gulp.watch('src/' + x + '/jsx/*.js', ['build-jsx-' + x])));
 
-gulp.task('watch-files', ['first-build'], () =>
-	gulp.watch(SRC_FILES, ['copy-files']));
+gulp.task('watch-files', ['build-all'], () =>
+	SRC_FILES.forEach(file =>
+		gulp.watch(file, ['copy-' + file])));
 
 gulp.task('watch-start', ['watch-jsx', 'watch-files']);
 
 gulp.task('default', ['watch-start']);
-
-/*
-const http = require('http');
-const fs   = require('fs');
-const path = require('path');
-gulp.task('web', cb =>
-	http.createServer((req, res) =>
-		fs.createReadStream(path.join(__dirname, 'dist',
-			req.url + (req.url.endsWith('/') ? 'index.html' : '')))
-		.on('error', err => (res.end(err + ''), console.error(err)))
-		.pipe(res)
-		.on('error', err => (res.end(err + ''), console.error(err)))
-	).listen(process.env.PORT || 3000, cb));
-*/
