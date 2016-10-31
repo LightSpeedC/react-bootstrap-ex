f310f87574313dee5c72eea9f776d324e8190d46
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-31 15:22:40 +0900

    revise algorithm

--- web/random-port.js
+++ web/random-port.js
@@ -2,8 +2,8 @@ var s = process.env.USERNAME;
 var cs = 0;
 for (var i = 0; i < s.length; ++i) {
 	//console.log(s[i], i, s.codePointAt(i));
-	cs += s.codePointAt(i) * i;
+	cs += s.codePointAt(i) * (i + 1);
 }
-var port = cs % 100 + 3000;
+var port = cs % 1000 + 3000;
 console.log('set PORT=' + port + '\r');
 console.log('set HOT_RELOAD_PORT=' + (port + 1) + '\r');
