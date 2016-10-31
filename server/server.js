8ca432ac005ea487857bdb29c0e89f53b0bca4d6
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-11-01 07:36:42 +0900

    express web server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
 'use strict';
 
-console.log(__filename);
+const express = require('express');
+const app = express();
+const serveIndex = require('serve-index');
+
+app.use(express.static(process.env.DIST || '.'));
+app.use(serveIndex(process.env.DIST || '.', {icons: true}));
+app.listen(process.env.PORT || 3000);
