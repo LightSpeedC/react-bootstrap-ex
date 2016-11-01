5b7cbe299b4edd5c5a6bebea720dcc3272420f8c
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-11-02 07:30:51 +0900

    add comment

--- server/server.js
+++ server/server.js
@@ -7,17 +7,18 @@ void function () {
 
 	const context = {};
 
+	// ボディーパーサー
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({extended: false}));
 
-	//======================================================
+	// XYZ API
 	app.use('/xyz/api', require('./xyz-api')(context));
 
-	//======================================================
+	// 静的ファイルとディレクトリ一覧
 	app.use(express.static(process.env.DIST || '.'));
 	app.use(serveIndex(process.env.DIST || '.', {icons: true}));
 
-	//======================================================
+	// ポートでサービス開始
 	const server = app.listen(process.env.PORT || 3000, function () {
 		const port = server.address().port;
 		console.log('Example app listening at http://localhost:%s', port);
