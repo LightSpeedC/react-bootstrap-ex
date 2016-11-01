7e198d5b96b996c006b58e2fc74d31654f3d9e15
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-11-01 19:11:16 +0900

    revise

--- src/xyz/pub/jsx/app.js
+++ src/xyz/pub/jsx/app.js
@@ -10,49 +10,88 @@ import AppFooter from './app-footer';
 import AppCenter from './app-center';
 
 import {Button} from 'react-bootstrap';
+import {FormGroup, FormControl, ControlLabel, HelpBlock} from 'react-bootstrap';
+import request from 'light-request';
+import aa from 'aa';
 
-class OurComponent extends React.Component {
+class MyComponent extends React.Component {
 	constructor(props) {
 		super(props);
-		if (this.state === undefined) this.state = {};
-		console.log('My1:', Object.getOwnPropertyNames(this));
-		console.log('My2:', Object.getOwnPropertyNames(this.constructor.prototype));
 		Object.getOwnPropertyNames(this.constructor.prototype)
-			.filter(p => p.startsWith('on') || p.startsWith('handle'))
+			.filter(p => (p.startsWith('on') || p.startsWith('handle')) &&
+				typeof this[p] === 'function')
 			.forEach(p => Object.defineProperty(this, p,
 				{configurable: true, value: this[p].bind(this)}));
 	}
 }
 
-class App extends OurComponent {
+const PrimaryButton = props =>
+	<Button bsStyle="primary" {...props}/>;
+const WarningButton = props =>
+	<Button bsStyle="warning" {...props}/>;
+const InfoButton = props =>
+	<Button bsStyle="info" {...props}/>;
+const TextControl = props =>
+	<FormControl type="text" {...props}/>;
+
+class App extends MyComponent {
 	constructor(props) {
 		super(props);
-		console.log('App1:', Object.getOwnPropertyNames(this));
-		console.log('App2:', Object.getOwnPropertyNames(this.constructor.prototype));
-		this.state = {counter: 0};
-		//this.onClick = this.onClick.bind(this);
+		this.state = {counter: 0, books: [], value: '', bookName: ''};
 	}
 	onPlus() {
-		//this.setState({counter: this.state.counter + 1});
 		this.setState(state => ({counter: state.counter + 1}));
 	}
 	onMinus() {
 		this.setState(state => ({counter: state.counter - 1}));
 	}
 	onReset() {
-		this.setState(state => ({counter: 0}));
+		this.setState({counter: 0});
+	}
+	componentWillMount() {
+		aa(this.getBooks())
+		.catch(err => console.error('getBooks:', err));
+	}
+	onBookAdd() {
+		aa(this.postBook())
+		.catch(err => console.error('postBook:', err));
+	}
+	*getBooks() {
+		const res = yield request.get('/xyz/api/books');
+		this.setState({books: res.body});
+	}
+	*postBook() {
+		yield request.post('/xyz/api/books', {name:this.state.bookName});
+		yield *this.getBooks();
+		this.setState({bookName: ''});
+	}
+  	getValidationState() {
+		const length = this.state.value.length;
+		return length > 10 ? 'success':
+			length > 5 ? 'warning':
+			length > 0 ? 'error' : undefined;
+	}
+	onChangeBookName(e) {
+		this.setState({bookName: e.target.value});
 	}
 	render() {
 		return <div>
+			<AppHeader />
 			counter: {this.state.counter}
 			<br/>
-			<Button bsStyle="primary" onClick={this.onPlus}>plus ++</Button>
-			<Button bsStyle="warning" onClick={this.onMinus}>minus --</Button>
-			<Button bsStyle="info" onClick={this.onReset}>reset</Button>
-			<AppHeader />
+			<PrimaryButton onClick={this.onPlus} children="plus ++"/>
+			<WarningButton onClick={this.onMinus} children="minus --"/>
+			<InfoButton onClick={this.onReset} children="reset"/>
 			<AppCenter />
-			<p>test p1</p>
-			{this.props.children}
+
+			{this.state.books.map(b => <div>{b.id}: {b.name}</div>)}
+			<form onSubmit={this.onBookAdd}>
+				<TextControl placeholder="本の名前"
+					onChange={this.onChangeBookName}
+					value={this.state.bookName}
+				/>
+				<PrimaryButton onClick={this.onBookAdd} children="book add"/>
+			</form>
 			<AppFooter />
 		</div>;
 	}
