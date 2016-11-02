a9fa7e2110cfae076e2f139eb9ed2b1edf287e18
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-11-03 07:11:37 +0900

    reduce and rename

--- server/server.js
+++ server/server.js
@@ -4,12 +4,9 @@ void function () {
 	const express = require('express'), app = express();
 	const bodyParser = require('body-parser');
 
-	const PORT = process.env.PORT || 3000;
-	const HOT_RELOAD_PORT = process.env.HOT_RELOAD_PORT || 3080;
-	const DIST = require('path').resolve(process.env.DIST || 'dist');
-	console.log('port:', PORT, 'hot-reload-port:', HOT_RELOAD_PORT, 'dist:', DIST);
-
-	const context = {port: PORT, hot_reload_port: HOT_RELOAD_PORT, dist: DIST};
+	const context = {port: process.env.PORT || 3000,
+		hot_reload_port: process.env.HOT_RELOAD_PORT || 3080,
+		dist: require('path').resolve(process.env.DIST || 'dist')};
 	console.log(context);
 
 	// ボディーパーサー
@@ -20,13 +17,13 @@ void function () {
 	app.use('/xyz/api', require('./xyz-api')(context));
 
 	// 静的ファイルとディレクトリ一覧
-	const onRequest = require('./web-serve-dist-hot-reload')(context);
+	const onRequest = require('./serve-hot-reload')(context);
 	app.use(onRequest);
 	// Hot Reload Service
 	onRequest.hotReloadService(context);
 
 	// ポートでサービス開始
-	const server = app.listen(PORT, function () {
+	const server = app.listen(context.port, function () {
 		const port = server.address().port;
 		console.log('Example app listening at http://localhost:%s', port);
 	});
