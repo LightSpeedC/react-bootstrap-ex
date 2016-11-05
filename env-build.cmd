4d37140285dc25abea2f7e0b78bffceb7740b158
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-11-05 23:35:59 +0900

    env-build - yarn, clean, gulp, server

--- env-build.cmd
+++ env-build.cmd
@@ -1,3 +1,3 @@
 @rem call yarn add --dev react react-dom react-router react-tap-event-plugin babel-preset-es2015 babel-preset-react babelify browserify literalify gulp run-sequence gulp-rename gulp-plumber gulp-uglify react-bootstrap react-tap-event-plugin vinyl-source-stream minimatch@^3 graceful-fs@^4
-yarn && start gulp && web
+yarn && gulp clean && start gulp && server
 pause
