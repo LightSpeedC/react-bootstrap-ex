207f290a83058db6a9053b8d7ad94585236e485a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-11-11 07:23:22 +0900

    revise

--- env-build.cmd
+++ env-build.cmd
@@ -1,3 +1,4 @@
 @rem call yarn add --dev react react-dom react-router react-tap-event-plugin babel-preset-es2015 babel-preset-react babelify browserify literalify gulp run-sequence gulp-rename gulp-plumber gulp-uglify react-bootstrap react-tap-event-plugin vinyl-source-stream minimatch@^3 graceful-fs@^4
+cd server && call yarn && cd ..
 yarn && gulp clean && start gulp && server
 pause
