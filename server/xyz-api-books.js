90e8a05f78298ecdcdd8f18933506c2400b18234
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-11-12 06:20:18 +0900

    simplify and add comments

--- server/xyz-api-books.js
+++ server/xyz-api-books.js
@@ -1,72 +1,69 @@
 module.exports = function (context) {
-	const data = {list: [{id:0,name:'book0'}, {id:1,name:'book1'}]};
-	const maxId = (a, b) => Math.max(a, b.id);
-	data.id = data.list.reduce(maxId, 0) + 1;
-	context.data = data;
-
-	for (let id = data.id; id < 1e3; ++id)
-		data.list.push({id, name: 'book' + id});
-	data.id = data.list.reduce(maxId, 0) + 1;
-	const SIZE = 10;
-
 	const router = require('express').Router();
 
-	const NOT_FOUND = {code: 'not_found',
-					message: 'data is not found'};
+	const data = context.data = {list: []};
+	const reduceMaxId = (a, b) => Math.max(a, b.id);
 
+	for (let id = 0; id < 1e3; ++id)
+		data.list.push({id, name: 'book' + id});
+
+	data.nextId = data.list.reduce(reduceMaxId, 0) + 1;
+
+	const SIZE = 10;
+	const NOT_FOUND = {code: 'not_found',
+			message: 'data is not found'};
+
+	// GET /?offset=0&size=10
 	router.get('/', (req, res) => {
 		const offset = Number(req.query.offset || 0);
 		const size = Number(req.query.size || SIZE);
 		resResult(res, data.list.slice(offset, offset + size));
 	});
 
+	// GET /:id
 	router.get('/:id', (req, res) => {
 		const pos = getIndex(Number(req.params.id));
-		if (pos >= 0)
-			resResult(res, data.list[pos]);
-		else
-			resError(res, NOT_FOUND);
+		if (pos < 0) return resError(res, NOT_FOUND);
+		resResult(res, data.list[pos]);
 	});
 
+	// POST /
 	router.post('/', (req, res) => {
 		const elem = req.body;
-		elem.id = data.id++;
+		elem.id = data.nextId++;
 		data.list.push(elem);
 		resResult(res, elem);
 	});
 
+	// PUT /:id
 	router.put('/:id', (req, res) => {
 		const pos = getIndex(Number(req.params.id));
-		if (pos >= 0) {
-			data.list[pos] = req.body;
-			resResult(res, true);
-		}
-		else
-			resError(res, NOT_FOUND);
+		if (pos < 0) return resError(res, NOT_FOUND);
+		resResult(res, data.list.splice(pos, 1, req.body));
 	});
 
+	// DELETE /:id
 	router.delete('/:id', (req, res) => {
 		const pos = getIndex(Number(req.params.id));
-		if (pos >= 0) {
-			data.list.splice(pos, 1);
-			resResult(res, true);
-		}
-		else
-			resError(res, NOT_FOUND);
+		if (pos < 0) return resError(res, NOT_FOUND);
+		resResult(res, data.list.splice(pos, 1));
 	});
 
+	// getIndex
 	function getIndex(id) {
-		const n = data.list.length;
-		for (let pos = 0; pos < n; ++pos)
+		const len = data.list.length;
+		for (let pos = 0; pos < len; ++pos)
 			if (data.list[pos].id === id)
 				return pos;
 		return -1;
 	}
 
+	// resResult
 	function resResult(res, result) {
 		res.json({result, length: data.list.length});
 	}
 
+	// resError
 	function resError(res, error) {
 		res.json({error, length: data.list.length});
 	}
